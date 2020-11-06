ae6aa4c5d93e23b8ecdcfe04b72fdf2e26e50ab0
Sergio Betancourt <45174963+sergiobet@users.noreply.github.com>
2020-11-06 13:31:55 -0400

    creación del service worker

---
+++ sw.js
@@ -0,0 +1,63 @@
+const MI_CACHE = 'cache-de-mi-app-1'; // El nombre de la caché que vamos a usar. 
+const archivos_para_cachear = [       // Array de las urls de los archivos que quiero guardar en caché
+  '/',                                
+  '/css/main.css',                                        
+  '/blog/post.html',                  
+  '/assets/img/foto.png'
+];
+
+self.addEventListener('install', event => {         // self es lo mismo que 'this'
+  
+  event.waitUntil(                                  // Recibe una promesa y actúa dependiendo de su resolución
+    caches.open(MI_CACHE)                           // ... Es claro qué hace
+      .then(cache => {
+        return cache.addAll(archivos_para_cachear); // Una vez se abre la caché se agregan a ella todos los archivos especificados
+      })
+  ).then(() => {
+    return self.skipWaiting();                      // El SW pasa de estado 'instalando' a 'activado'
+  });
+
+});
+self.addEventListener('activate', event => {   // Escuchamos al evento 'activate'
+  event.waitUntil(self.clients.claim());        // El SW se registra como el worker activo para el cliente actual 
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {          // Toma las caches existentes
+
+      return Promise.all(
+        cacheNames.map(cacheName => {           // Recorremos las caches exitentes
+          if (MI_CACHE !== cacheName) {     // Si la caché del recorrido no es la caché actual...  
+            return caches.delete(cacheName);    // La borramos, así conservamos únicamente la más reciente
+          }
+        })
+      );
+
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {    // Escuchamos al evento 'fetch',
+                                             //  este se ejecuta siempre que se hace una solicitud HTTP (se pide o envía algo por Internet)
+  event.respondWith(
+    caches.open(MI_CACHE).then(cache => {               // Abrimos la caché (en este momento ya contiene los archivos que decidimos cachear)
+      return fetch(event.request).then(fetchResponse => {   // event.request es la solicitud al recurso. Contiene la URL y el método utilizado
+
+        if(event.request.method === 'GET'){                 // Si el método es GET, quiere decir que estamos intentando traer datos,   
+          cache.put(event.request, fetchResponse.clone());  //entonces interceptamos la respuesta y la agregamos a MI_CACHE
+        }
+
+        return fetchResponse;                 // Después dejamos que la solicitud siga su curso
+
+      }).catch( _ => {                        // .catch se ejecutará cuando no se pueda hacer un 'fetch', en otras palabras,
+                                              //   cuando no se pueda completar una solicitud HTTP a Internet (offline)
+
+        return cache.match(event.request)     // cache.match intenterá encontrar un archivo que cumpla con las características del recurso solicitado
+          .then(cacheResponse => cacheResponse);  // Y después enviamos ese archivo encontrado en caché como respuesta.
+
+      })
+
+    })
+
+  );
+
+});
